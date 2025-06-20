06cc51647e7fe32ff0fa9bbd53ff58a1c1b548ad
Stive <zhuo-chan-stive.lee@efrei.net>
2025-06-20 18:25:53 +0200

    Ajout du frontend angular

---
+++ frontend/src/app/components/depense-list/depense-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-depense-list',
+  imports: [],
+  templateUrl: './depense-list.html',
+  styleUrl: './depense-list.css'
+})
+export class DepenseList {
+
+}
